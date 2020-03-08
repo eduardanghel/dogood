14e719f006a14f8271e2be136454d1ca87b8fbf1
Oscar <oscar.pinton@gmail.com>
2020-03-08 14:30:53 +0000

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,137 +1,115 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TextInput, Button, TouchableOpacity } from 'react-native';
+
 import { LinearGradient } from 'expo-linear-gradient';
 
-import WelcomeToKarma from "./components/WelcomeToKarma.js";
-import Login from "./components/Login.js";
-import ForgotPass from "./components/ForgotPass.js";
-import UserRegistration from "./components/UserRegistration.js";
-import OpenEmail from "./components/OpenEmail.js";
-import Feed from "./components/Feed.js";
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.state = { password: '' };
+  }
 
-function InitialScreen({ navigation }) {
-  return (
-
-    <View style={styles.container}>
-      <LinearGradient
-        style={{ alignItems: 'center', justifyContent: 'center', flex: 1, width: '100%' }}
-        colors={['#00c5c4', '#01a7a6']}
-        start={{ x: 1, y: 0 }}
-        end={{ x: 0, y: 0 }}>
-        <Text style={styles.textHeader}>KARMA</Text>
-        <Text style={styles.text}>Lorem ipsum dolo ecte </Text>
-        <Text style={styles.text}>adipisicing elit sed do</Text>
-        <TouchableOpacity
-          style={styles.buttonContainer}
-          onPress={() => navigation.navigate('Back')}
-        >
-          <Text style={{ color: "white", fontSize: 20, }}>Sign Up</Text>
-        </TouchableOpacity>
-        <Text
-          style={styles.loginText}
-          onPress={() => navigation.navigate('Loginate')}
-        >
-          Already have an account? Login
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <View>
+          {/*<Text style={styles.paragraph}>
+            Login
             </Text>
-      </LinearGradient>
-    </View>
-  );
-}
+    */}
+          <Text style={styles.text}>
+            Welcome back!
+            </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email Address"
+            placeholderTextColor='grey'
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor='grey'
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+          />
+        </View>
 
-const Stack = createStackNavigator();
+        <View style={{ flex: 1, marginLeft: 170 }} >
+          <Button
+            title="Forgot Password?"
+            fontSize='10'
+            color='#01b0b0'
+            onPress={() => navigate('ForgotPass')}
+          />
+        </View>
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={InitialScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Back" component={WelcomeToKarma} options={{ headerShown: false }} />
-        <Stack.Screen name="UserRegistration"
-          component={UserRegistration}
-          options={{
-            headerTintColor: '#01b0b0',
-            title: 'Sign up',
-            headerTitleStyle: {
-              textAlign: 'left',
-              fontWeight: 'bold',
-              fontSize: 22,
-              color: 'black',
-            }
-          }} />
-        <Stack.Screen name="Loginate" component={Login}
-          options={{
-            headerTintColor: '#01b0b0',
-            title: 'Login',
-            headerTitleStyle: {
-              textAlign: 'left',
-              color: 'black',
-              fontSize: 22,
-            }
-          }} />
-        <Stack.Screen name="ForgotPass" component={ForgotPass}
-          options={{
-            headerTintColor: '#01b0b0',
-            title: 'Forgot Password',
-            headerTitleStyle: {
-              textAlign: 'left',
-              color: 'black',
-              fontSize: 22,
-            }
-          }} />
-        <Stack.Screen name="OpenEmail" component={OpenEmail}
-          options={{
-            headerTintColor: '#01b0b0',
-            title: "Forgot Password",
-            headerTitleStyle: {
-              textAlign: 'left',
-              color: 'black',
-              fontSize: 22,
-            }
-          }} />
-          <Stack.Screen name="Feed" component={Feed} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+        <View style={{ flex: 3, justifyContent: 'space-evenly', alignItems: 'center', marginVertical: 24 }}>
+          <LinearGradient
+            style={styles.button}
+            colors={['#00c5c4', '#01a7a6']}
+            start={{ x: 1, y: 0 }}
+            end={{ x: 0, y: 0 }}
+          >
+            <TouchableOpacity onPress={() => navigate('Feed')}><Text style={styles.loginButton}>Login</Text></TouchableOpacity>
+          </LinearGradient>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 8,
   },
-  textHeader: {
-    color: 'white',
-    fontSize: 80,
+  paragraph: {
+    margin: 24,
+    fontSize: 22,
+    fontWeight: '500',
+    //textAlign: 'left',
+    //position: 'absolute',
+    left: 27,
+    marginLeft: -60,
+    marginBottom: 10
   },
   text: {
+    margin: 25,
+    fontSize: 22,
+    fontWeight: '500',
+    textAlign: 'left',
+    color: '#01b0b0',
+
+  },
+  input: {
+    height: 50,
+    padding: 10,
+    margin: 18,
+    borderBottomColor: '#D3D3D3',
+    borderBottomWidth: 1,
+
+  },
+  button: {
+    marginLeft: 15,
+    borderRadius: 22,
+    //borderTopRightRadius: 30,
+    //borderBottomRightRadius: 30,
+    //borderTopLeftRadius: 30,
+    //borderBottomLeftRadius: 30,
+    width: 300,
+    paddingHorizontal: 16,
+  },
+  loginButton: {
+    fontSize: 16,
     color: 'white',
-    fontSize: 20,
-  },
-  buttonContainer: {
-    borderColor: 'white',
-    borderWidth: 2,
-    borderRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    height: 44,
-    width: 200,
-    marginBottom: 20,
-    marginTop: 10,
-    position: 'absolute',
-    bottom: 60,
-  },
-  loginText: {
-    color: "white",
-    fontSize: 15,
-    marginTop: 10,
-    marginBottom: 20,
-    bottom: 10,
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 12,
+    marginBottom: 12,
+  }
 });
