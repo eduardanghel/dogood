0878e97389f250ec3367171fd8d030beed544bf4
nathank1894 <55813157+nathank1894@users.noreply.github.com>
2020-03-15 04:05:26 +0000

    refactor CreateActivity

--- frontend/components/CreateActivity.js
+++ frontend/components/CreateActivity.js
@@ -18,171 +18,164 @@ export default class App extends React.Component {
     render() {
       return (
         <ScrollView>
-         <View>    
-            <Text style={styles.text}>Create Activity</Text>
-            <Text style={styles.de}>
+            <View>    
+                <Text style={styles.text}>Create Activity</Text>
+                <Text style={styles.de}>
                     Lorem ipsum dolor sit amet, conse ct etur adip isicing do eiut, sunt in culpa
-            </Text>
-            <Text style={styles.add}>
-            Add photo
-            </Text>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', }}>
-              <TouchableOpacity style={styles.cam}><Icons name={'camera-alt'} size={20} color='white' /></TouchableOpacity>
-              <TouchableOpacity><Text style={styles.upload}>Upload Photo</Text></TouchableOpacity>
-  
+                </Text>
+                <Text style={styles.add}>
+                    Add photo
+                </Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', }}>
+                    <TouchableOpacity style={styles.cam}><Icons name={'camera-alt'} size={20} color='white' /></TouchableOpacity>
+                    <TouchableOpacity><Text style={styles.upload}>Upload Photo</Text></TouchableOpacity>
+                </View>
+                <DatePicker 
+                    showIcon={true}
+                    style={styles.dat}
+                    date={this.state.startDate}
+                    iconSource=''
+                    mode="date"
+                    placeholder="Start date"
+                    format="YYYY-MM-DD"
+                    minDate="2020-01-01"
+                    maxDate="2100-01-01"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                            },
+                        dateInput: {
+                            marginLeft: 36,
+                            borderWidth: 0,
+                            borderBottomWidth:1,
+                            }
+         
+                    }}
+                    onDateChange={(startDate) => {this.setState({startDate: startDate})}}
+                />
+                <DatePicker 
+                    showIcon={false}
+                    style={styles.dat}
+                    date={this.state.endDate}  
+                    mode="date"
+                    placeholder="End date"
+                    format="YYYY-MM-DD"
+                    minDate="2020-01-01"
+                    maxDate="2100-01-01"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                        },
+                        dateInput: {
+                            marginLeft: 36,
+                            borderWidth: 0,
+                            borderBottomWidth:1,
+                        }
+                    }}
+                    onDateChange={(endDate) => {this.setState({endDate: endDate})}}
+                />
+                <DatePicker
+                    style={styles.dat}
+                    date={this.state.startTime}
+                    mode="time"
+                    showIcon={false}
+                    format="HH:mm"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    placeholder="Start time"
+                    minuteInterval={10}
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                        },
+                        dateInput: {
+                            marginLeft: 36,
+                            borderWidth: 0,
+                            borderBottomWidth:1,   
+                        }
+                     }}
+                     onDateChange={(startTime) => {this.setState({startTime: startTime})}}
+                />
+                <DatePicker
+                    style={styles.dat}
+                    date={this.state.endTime}
+                    mode="time"
+                    showIcon={false}
+                    format="HH:mm"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    placeholder="End time"
+                    minuteInterval={10}
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0
+                        },
+                        dateInput: {
+                            marginLeft: 36,
+                            borderWidth: 0,
+                            borderBottomWidth:1,    
+                        }
+                    }}
+                    onDateChange={(endTime) => {this.setState({endTime: endTime});}}
+                />
+                <Text style={styles.text2}>Address </Text>
+                <Text style={styles.text3}>Please leave this blank if you will be sending this via email once a volunteer has confirm </Text>
+                <View style={styles.container}>
+                    <TextInput style={styles.textinput} editable={false} placeholder="Make address visible" />
+                    <Switch
+                        style={styles.switc}
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}/>
+                </View>
+                <TextInput style={styles.textinput2} editable={true} placeholder="What will volunteers do?" />
+                <TextInput style={styles.textinput2} editable={true} placeholder="Who to contact?" />
+                <TextInput style={styles.textinput2} editable={true} placeholder="Where?" />
+                <Text style={styles.im}>Important </Text> 
+                <View style={styles.container}>
+                    <TextInput style={styles.textinput3} editable={false} placeholder="Minimun age is 18" />
+                    <Switch
+                        style={styles.swit}
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}/>
+                </View>
+                <View style={styles.container}>
+                    <TextInput style={styles.textinput3} editable={false} placeholder="Photo ID required" />
+                    <Switch
+                        style={styles.swit}
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}/>
+                </View>
+                <View style={styles.container}>
+                    <TextInput style={styles.textinput3} editable={false} placeholder="This is a physical activity" />
+                   <Switch
+                        style={styles.swit}
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}/>
+                </View>
+                <View style={styles.container}>
+                    <TextInput style={styles.textinput3} editable={false}  multiline ={true}placeholder="Additional information will be provided by email" />
+                    <Switch
+                        style={styles.swit}
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}/>
+                </View>
             </View>
-           <DatePicker 
-        showIcon={true}
-        style={styles.dat}
-        date={this.state.startDate}
-        iconSource=''
-        mode="date"
-        placeholder="Start date"
-        format="YYYY-MM-DD"
-        minDate="2020-01-01"
-        maxDate="2100-01-01"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          dateIcon: {
-            position: 'absolute',
-            left: 0,
-            top: 4,
-            marginLeft: 0
-          },
-          dateInput: {
-            marginLeft: 36,
-            borderWidth: 0,
-            borderBottomWidth:1,
-          }
-          // ... You can check the source to find the other keys.
-        }}
-        onDateChange={(startDate) => {this.setState({startDate: startDate})}}
-      />
-      <DatePicker 
-        showIcon={false}
-        style={styles.dat}
-        date={this.state.endDate}
-        
-        mode="date"
-        placeholder="End date"
-        format="YYYY-MM-DD"
-        minDate="2020-01-01"
-        maxDate="2100-01-01"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          dateIcon: {
-            position: 'absolute',
-            left: 0,
-            top: 4,
-            marginLeft: 0
-          },
-          dateInput: {
-            marginLeft: 36,
-            borderWidth: 0,
-            borderBottomWidth:1,
-            
-          }
-          
-          // ... You can check the source to find the other keys.
-        }}
-        onDateChange={(endDate) => {this.setState({endDate: endDate})}}
-      />
-       <DatePicker
-          style={styles.dat}
-          date={this.state.startTime}
-          mode="time"
-          showIcon={false}
-          format="HH:mm"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          placeholder="Start time"
-          minuteInterval={10}
-          customStyles={{
-          dateIcon: {
-            position: 'absolute',
-            left: 0,
-            top: 4,
-            marginLeft: 0
-          },
-          dateInput: {
-            marginLeft: 36,
-            borderWidth: 0,
-            borderBottomWidth:1,
-            
-          }
-          }}
-          onDateChange={(startTime) => {this.setState({startTime: startTime})}}
-        />
-        <DatePicker
-          style={styles.dat}
-          date={this.state.endTime}
-          mode="time"
-          showIcon={false}
-          format="HH:mm"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          placeholder="End time"
-          minuteInterval={10}
-          customStyles={{
-          dateIcon: {
-            position: 'absolute',
-            left: 0,
-            top: 4,
-            marginLeft: 0
-          },
-          dateInput: {
-            marginLeft: 36,
-            borderWidth: 0,
-            borderBottomWidth:1,
-            
-          }
-          }}
-          onDateChange={(endTime) => {this.setState({endTime: endTime});}}
-        />
-        <Text style={styles.text2}>Address </Text>
-        <Text style={styles.text3}>Please leave this blank if you will be sending this via email once a volunteer has confirm </Text>
-          <View style={styles.container}>
-         <TextInput style={styles.textinput} editable={false} placeholder="Make address visible" />
-        <Switch
-          style={styles.switc}
-          onValueChange = {this.toggleSwitch}
-          value = {this.state.switchValue}/>
-      </View>
-      <TextInput style={styles.textinput2} editable={true} placeholder="What will volunteers do?" />
-       <TextInput style={styles.textinput2} editable={true} placeholder="Who to contact?" />
-       <TextInput style={styles.textinput2} editable={true} placeholder="Where?" />
-       <Text style={styles.im}>Important </Text> 
-       <View style={styles.container}>
-         <TextInput style={styles.textinput3} editable={false} placeholder="Minimun age is 18" />
-        <Switch
-          style={styles.swit}
-          onValueChange = {this.toggleSwitch}
-          value = {this.state.switchValue}/>
-      </View>
-      <View style={styles.container}>
-         <TextInput style={styles.textinput3} editable={false} placeholder="Photo ID required" />
-        <Switch
-          style={styles.swit}
-          onValueChange = {this.toggleSwitch}
-          value = {this.state.switchValue}/>
-      </View>
-      <View style={styles.container}>
-         <TextInput style={styles.textinput3} editable={false} placeholder="This is a physical activity" />
-        <Switch
-          style={styles.swit}
-          onValueChange = {this.toggleSwitch}
-          value = {this.state.switchValue}/>
-      </View>
-      <View style={styles.container}>
-         <TextInput style={styles.textinput3} editable={false}  multiline ={true}placeholder="Additional information will be provided by email" />
-        <Switch
-          style={styles.swit}
-          onValueChange = {this.toggleSwitch}
-          value = {this.state.switchValue}/>
-      </View>
-         </View>
         </ScrollView>
       );
     }
@@ -220,7 +213,6 @@ export default class App extends React.Component {
         marginLeft: -22,
     },
     upload: {
-
         borderWidth: 1,
         color: 'gray',
         borderColor: 'gray',
@@ -230,7 +222,6 @@ export default class App extends React.Component {
         textAlign: 'center',
         paddingTop: 10,
         // marginLeft:10,
-     
     },
     dat: {
         showIcon: false,
@@ -241,12 +232,12 @@ export default class App extends React.Component {
         borderWidth: 0,
     },
     container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    paddingVertical: 1,
-    paddingHorizontal: 1,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingVertical: 1,
+        paddingHorizontal: 1,
     },
     text2: {
         fontSize: 16,
@@ -263,62 +254,62 @@ export default class App extends React.Component {
 
     },
     textinput: {
-    fontSize: 16,
-    color: 'grey',
-    borderBottomColor: 'grey',
-    borderTopColor: 'grey',
-    borderBottomWidth: 1,
-    borderTopWidth:1,
-    paddingVertical: 10,
-    marginVertical: 2,
-    width: 250,
-    marginLeft: 30,
-    marginTop: 41,
-    padding:1,
-    paddingTop: 10,
-    paddingRight: 10,
-    paddingBottom: 10,
-    paddingLeft: 0,
+        fontSize: 16,
+        color: 'grey',
+        borderBottomColor: 'grey',
+        borderTopColor: 'grey',
+        borderBottomWidth: 1,
+        borderTopWidth:1,
+        paddingVertical: 10,
+        marginVertical: 2,
+        width: 250,
+        marginLeft: 30,
+        marginTop: 41,
+        padding:1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
     },
     textinput2: {
-      fontSize: 16,
-    color: 'grey',
-    borderBottomColor: 'grey',
-    borderBottomWidth: 1,
-    width: 250,
-    marginLeft: 51,
-    paddingVertical: 10,
-    marginVertical: 2,
+        fontSize: 16,
+        color: 'grey',
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        width: 250,
+        marginLeft: 51,
+        paddingVertical: 10,
+        marginVertical: 2,
     },
     textinput3: {
-    fontSize: 16,
-    color: 'grey',
-    borderBottomColor: 'grey',
-    borderTopColor: 'grey',
-    //borderBottomWidth: 1,
-    paddingVertical: 10,
-    marginVertical: 2,
-    width: 250,
-    marginLeft: 41,
-    marginTop: 15,
-    padding:1
+        fontSize: 16,
+        color: 'grey',
+        borderBottomColor: 'grey',
+        borderTopColor: 'grey',
+        //borderBottomWidth: 1,
+        paddingVertical: 10,
+        marginVertical: 2,
+        width: 250,
+        marginLeft: 41,
+        marginTop: 15,
+        padding:1
     },
     im: {
-      fontSize: 16,
-      marginLeft: 51,
-      color: 'black',
-      fontWeight: 'bold',
-      marginTop: 20,
-      marginBottom: 1,
+        fontSize: 16,
+        marginLeft: 51,
+        color: 'black',
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 1,
     },
     swit: {
-      marginTop:20,
-      transform: [{ scaleX: .9 }, { scaleY: .9 }],
+        marginTop:20,
+        transform: [{ scaleX: .9 }, { scaleY: .9 }],
     },
     switc: {
-      marginTop:30,
-      transform: [{ scaleX: .9 }, { scaleY: .9 }],
-      padding: 10,
+        marginTop:30,
+        transform: [{ scaleX: .9 }, { scaleY: .9 }],
+        padding: 10,
     }
   });
 
