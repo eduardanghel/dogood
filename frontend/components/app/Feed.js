1723c96d0bab4ad619ae7a136432f91be7624103
ShaneCpc <34100494+ShaneCpc@users.noreply.github.com>
2020-04-05 00:20:54 +0100

    Updated profile screen to profile feed

--- frontend/components/app/Feed.js
+++ frontend/components/app/Feed.js
@@ -9,7 +9,7 @@ import Icon from 'react-native-vector-icons/EvilIcons';
 
 import Activities from './Activities';
 import Notifications from './Notifications';
-import ProfileScreen from './profile/EditProfile';
+import ProfileScreen from './profile/UserProfileFeed';
 import MyCauses from './MyCauses'
 
 import COLORS from '../reusables/Colors';
