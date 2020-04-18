4d9ee3115cc6120a7ac919cfe335c0bc1eb17203
Eduard Anghel <k1631379@kcl.ac.uk>
2020-04-19 00:44:48 +0100

    Lint

--- frontend/__tests__/App-test.js
+++ frontend/__tests__/App-test.js
@@ -1,15 +1,15 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-import App from '../App';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// /**
+//  * @format
+//  */
+//
+// import 'react-native';
+// import React from 'react';
+//
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+//
+// import App from '../App';
+//
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
